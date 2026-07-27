427efa4581dee29d48940a1f159ec4a1e98b94bc
AraiShia <Arai.Shia@outlook.com>
2026-07-27 09:32:23 +0800

    chore: 忽略 .venv/ client/ dist/ build/; backend/main.py -> server.py

--- client/dialogs/purchase.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""purchase 相关 Dialog（延迟加载）"""
-
-class PurchaseDialog:
-    def __init__(self, api_client, dept_id, purchase=None):
-        from main import PurchaseDialog as PUD
-        self._dialog = PUD(api_client, dept_id, purchase)
-    def exec(self):
-        return self._dialog.exec()
