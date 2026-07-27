9701ca1e4365ab79a06c8f8c1cd590ac8a0a586f
AraiShia <Arai.Shia@outlook.com>
2026-07-27 10:17:21 +0800

    chore: 从仓库移除 client/，加入 gitignore
    client/ 已迁移到独立 Git 仓库管理。

--- client/web_container/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Web 容器模块：QWebEngineView + QWebChannel 桥接"""
-from .web_view import WebContainerView
-from .channel_bridge import NativeBridge
-
-__all__ = ['WebContainerView', 'NativeBridge']
--- client/widgets/payment/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-付款相关组件包
-"""
-
-from .water_bill_viewer import WaterBillViewer
-
-__all__ = ['WaterBillViewer']
